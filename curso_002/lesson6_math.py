8de137ae04b94482aee324b05ef91f19acffc101
Fabio Kfouri <kfouri.fabio@gmail.com>
2026-01-02 13:26:55 -0300

    generate and read mid

--- curso_002/lesson6_math.py
+++ curso_002/lesson6_math.py
@@ -9,12 +9,32 @@ scale = Scale.melodic_minor(62)
 
 instr = s.new_part("strings")
 
+s.start_transcribing()
+
+i = 0
 while True:
-    raw_pitch = 80 + 10 * math.sin(s.beat())
+    raw_pitch = 80 + 10 * math.sin(s.beat() )
     raw_pitch = 80 + 10 * math.sin(100 / (0.1 + s.beat()))
 
     pitch = scale.round(raw_pitch)
 
-    instr.play_note(pitch, 0.8, 0.25, "stacatto")
+    instr.play_note(pitch, 0.8, 0.25)
 
     print(f"{raw_pitch}:{pitch}", end=" ")
+
+    i+=1
+    if i > 30:
+        break
+
+s.wait(1)
+s
+
+performance = s.stop_transcribing()
+# performance.to_score(
+#     QuantizationScheme.from_time_signature("3/4", 16),
+#     title="Teste Kfouri",
+#     composer="Last Nobler",
+# ).show()
+
+# performance.to_midi("test.mid")
+performance.export_to_midi_file("./music/scamp_test.mid")
---
+++ curso_002/read1.py
@@ -0,0 +1,20 @@
+from scamp import *
+from scamp_extensions.parsing.midi import scrape_midi_file_to_dict
+from __init__ import JELOUS_GUY_MIDI
+
+midi_data = scrape_midi_file_to_dict(JELOUS_GUY_MIDI)
+
+print(midi_data)
+
+
+s = Session()
+s.tempo = 110
+
+piano = s.new_part("piano")
+
+for wait_time, pitch, length, volume in zip(midi_data['inter_onset_times'],
+                                            midi_data['pitches'],
+                                            midi_data['lengths'],
+                                            midi_data['volumes']):
+    piano.play_note(pitch, volume, length, blocking=False)
+    wait(wait_time)
